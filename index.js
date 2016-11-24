b4fdeff4ab689b4fcf5076073b49f1622e809b20
Aftab Alam <aftabbuddy@gmail.com>
2016-11-24 15:39:23 +0530

    Add symbols for popular entities

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const readline = require('readline');
+const stock = require('./lib/stock');
+const mapify = require('./lib/mapify');
+const stockSymbols = mapify(require('./data/symbols'));
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
@@ -6,10 +9,11 @@ process.stdin.on('keypress', (str, key) => {
     if (key.ctrl && key.name === 'c') {
         process.exit();
     } else {
-        console.log(`You pressed the "${str}" key`);
-        console.log();
-        console.log(key);
-        console.log();
+        if (stockSymbols.has(str)) {
+            console.log(stock.getStockQuote(str));
+        } else {
+            console.log('No symbol defined for "${str}" key')
+        }
     }
 });
 
